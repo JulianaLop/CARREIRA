caf0bae9ecc96039842396829467fef9895b796c
JulianaLop <juliana.inacio@etec.sp.gov.br>
2023-09-05 13:01:17 -0300

    atualizado

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/Router.php';
-require_once __DIR__ . '/templates/Template.php';
+require_once __DIR__ . '/templates/template.php';
 $request = $_SERVER['REQUEST_URI'];
 
 
